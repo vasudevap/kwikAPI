82c391f48e00f93b6fa643969426c453e96987c4
PV <140992905+vasudevap@users.noreply.github.com>
2023-09-19 18:44:50 -0400

    ADDED category model

--- models/Category.js
+++ models/Category.js
@@ -12,30 +12,10 @@ Category.init(
       primaryKey: true,
       autoIncrement: true
     },
-    trip_budget: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: true
-    },
-    traveller_amount: {
-      type: DataTypes.INTEGER,
+
+    category_name: {
+      type: DataTypes.STRING,
       allowNull: false,
-      defaultValue: 1
-    },
-    traveller_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'traveller',
-        key: 'id',
-        unique: false
-      }
-    },
-    location_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'location',
-        key: 'id',
-        unique: false
-      }
     }
   },
   {
