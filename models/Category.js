fedfc6ff2d068dfe61aeb361042cd310c93a5571
PV <140992905+vasudevap@users.noreply.github.com>
2023-09-19 16:51:38 -0400

    ADDED starter code for product category and index MODELS

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,37 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    trip_budget: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: true
+    },
+    traveller_amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1
+    },
+    traveller_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'traveller',
+        key: 'id',
+        unique: false
+      }
+    },
+    location_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'location',
+        key: 'id',
+        unique: false
+      }
+    }
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -5,7 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Traveller.belongsToMany(Location, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: Trip,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'planned_trips'
+});
 
+Location.belongsToMany(Traveller, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: Trip,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'location_travellers'
+});
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
